a24c1a8de4ca40fa046e4dc273ba93168ee9102d
roundedrectangle <kozelderezel12345678@gmail.com>
2025-05-17 19:58:39 +0300

    repository.from_url

--- python/repository.py
+++ python/repository.py
@@ -1,8 +1,7 @@
 from __future__ import annotations
 
-from hashlib import sha256
-
 from attrs import define
+import httpx
 
 from utility import Utility
 from utils import *
@@ -16,3 +15,8 @@ class Repository:
     @classmethod
     def from_json(cls, data):
         return load_model(data, cls, 'repository')
+    
+    @classmethod
+    def from_url(cls, url, client: httpx.Client):
+        data = client.get(url).content
+        return cls.from_json(data)
