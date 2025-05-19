6d4fdef8fd532e839200e835f4bdb6a0f265bf38
roundedrectangle <kozelderezel12345678@gmail.com>
2025-05-19 20:32:42 +0300

    Fix typing

--- python/main.py
+++ python/main.py
@@ -23,14 +23,16 @@ def set_proxy(proxy):
         client.close()
     client = httpx.Client(proxy=convert_proxy(proxy))
 
-def set_constants(*args):
+def set_constants(_data, _cache):
     global data, cache, repos_manager
-    data, cache = args
+    data, cache = Path(_data), Path(_cache)
     repos_manager = RepositoriesManager(data)
 
 
 def add_repo(url):
-    repos_manager.add_repo_from_url(url, client)
+    if repos_manager:
+        repos_manager.add_repo(url)
 
 def remove_repo(url):
-    repos_manager.remove_repo(url)
+    if repos_manager:
+        repos_manager.remove_repo(url)
