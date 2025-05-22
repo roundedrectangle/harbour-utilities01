c72f43482c2eae96843a4b57d9269f298b6ff21f
roundedrectangle <kozelderezel12345678@gmail.com>
2025-05-22 17:40:58 +0300

    reposmanager: add proper repos loading with caching

--- python/reposmanager.py
+++ python/reposmanager.py
@@ -1,6 +1,6 @@
 from __future__ import annotations
 
-from typing import Dict, List, Set
+from typing import TYPE_CHECKING, List, Set
 
 from utils import *
 from cattrsconfigbase import CattrsConfigBase
@@ -8,12 +8,22 @@ from repository import Repository
 
 from pyotherside_utils import *
 
+if TYPE_CHECKING:
+    from caching import Cacher
+
 class RepositoriesManager(CattrsConfigBase):
     _name = 'repos'
     _default_factory = set
     _data: set[str]
     _model = Set[str]
     _unstructure_as = List[str] # JSON does not support sets
+
+    _repos_cache: dict[str, Repository]
+
+    def __init__(self, location, cacher: Cacher):
+        super().__init__(location)
+        self.cacher = cacher
+        self._repos_cache = {}
 
     @property
     def repos(self):
@@ -28,3 +38,13 @@ class RepositoriesManager(CattrsConfigBase):
             self.repos.remove(url)
             self.save()
             return url
+    
+    def load_repo(self, url: str, force=False, force2=False):
+        hashed = sha256(url)
+        if force or hashed not in self._repos_cache:
+            self._repos_cache[hashed] = Repository.from_json(self.cacher.cache(url, force=force2))
+        return self._repos_cache[hashed]
+    
+    def __iter__(self):
+        for r in self.repos:
+            yield self.load_repo(r)
