212f829ea0800667d9a7dcc0280667849f64c071
roundedrectangle <kozelderezel12345678@gmail.com>
2025-05-22 19:17:52 +0300

    fix typing

--- python/utils.py
+++ python/utils.py
@@ -1,15 +1,16 @@
 from __future__ import annotations
 
-from pyotherside import send as qsend
-
+from typing import TypeVar
 import json
 
 from pyotherside_utils import *
 import cattrs
 
+V = TypeVar('V')
+
 cattrs_safe = lambda name='model': exception_safe({cattrs.ClassValidationError: ExceptionHandlingInfo(name, lambda e: ', '.join(cattrs.transform_error(e)))})
 
-def load_model(_data: str | dict, model: type[T], error_name=None) -> T:
+def load_model(_data: str | dict, model: type[V], error_name=None) -> V:
     @json_safe(f'json-{error_name}' if error_name else 'json')
     @cattrs_safe(f'model-{error_name}' if error_name else 'model')
     def wrapper():
