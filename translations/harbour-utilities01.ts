f1dd7b0d165c657b887d55e42415a85297b0c4c5
roundedrectangle <kozelderezel12345678@gmail.com>
2025-05-22 20:44:33 +0300

    translations update

--- translations/harbour-utilities01.ts
+++ translations/harbour-utilities01.ts
@@ -9,17 +9,84 @@
     </message>
 </context>
 <context>
-    <name>FirstPage</name>
+    <name>Errors</name>
     <message>
-        <source>Show Page 2</source>
+        <source>Python error</source>
         <translation type="unfinished"></translation>
     </message>
+    <message>
+        <source>Unable to load config &apos;%1&apos;: invalid JSON data. Resetting to default</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Unable to load config &apos;%1&apos;: insufficient permissions</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Unable to save config &apos;%1&apos;: insufficient permissions</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Unable to save config &apos;%1&apos;: file not found</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Unable to save config &apos;%1&apos;: invalid JSON data.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Unable to create directory for config &apos;%1&apos;: insufficient permissions</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Unable to load config &apos;%1&apos;: could not load cattrs model. Resetting to default</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Unknown error: %1</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>This usually means that configuration directory could not be created</source>
+        <comment>Description for &apos;Unable to save config %1: file not found&apos;</comment>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>FirstPage</name>
     <message>
         <source>UI Template</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
         <source>Hello Sailors</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Settings</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Repositories</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>ReposPage</name>
+    <message>
+        <source>Add repo</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Repositories</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Remove</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Removed repository</source>
         <translation type="unfinished"></translation>
     </message>
 </context>
@@ -31,6 +98,91 @@
     </message>
     <message>
         <source>Item</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>SettingsPage</name>
+    <message>
+        <source>Never</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>On app restart</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Hourly</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Daily</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Weekly</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Monthly</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Half-yearly</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Yearly</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Changes how often the cache is updated. App restart might be required</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Settings</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Debugging</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Show info messages in notifications</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>WelcomePage</name>
+    <message>
+        <source>Welcome to Utilities!</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Select default repositories</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>harbour-utilities01</name>
+    <message>
+        <source>Unknown error. This should not happen</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Unknown JSON decode error</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Unknown cattrs model construction error</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Invalid JSON in a repository</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Invalid model in a repository.</source>
         <translation type="unfinished"></translation>
     </message>
 </context>
