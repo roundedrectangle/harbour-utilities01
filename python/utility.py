aed926bd0f5a72e5ce7e0e58162eea60c63b7ad5
roundedrectangle <kozelderezel12345678@gmail.com>
2025-05-23 09:32:40 +0300

    add utility qml_data

--- python/utility.py
+++ python/utility.py
@@ -9,3 +9,10 @@ class Utility:
     # we can't use __future__.annotations in typing here
     name: str
     description: Optional[str] = None
+
+    @property
+    def qml_data(self):
+        return {
+            'name': self.name,
+            'description': self.description,
+        }
