af26e732ae74dfdd71022c1ffec02dfa9ed3d71a
roundedrectangle <roundedrectangle0@gmail.com>
2025-07-03 18:56:49 +0300

    use an internal uri for internal C++ types

--- src/harbour-utilities01.cpp
+++ src/harbour-utilities01.cpp
@@ -12,28 +12,18 @@
 
 #include "logic.h"
 
-int main(int argc, char *argv[])
-{
-    // SailfishApp::main() will display "qml/harbour-utilities01.qml", if you need more
-    // control over initialization, you can use:
-    //
-    //   - SailfishApp::application(int, char *[]) to get the QGuiApplication *
-    //   - SailfishApp::createView() to get a new QQuickView * instance
-    //   - SailfishApp::pathTo(QString) to get a QUrl to a resource file
-    //   - SailfishApp::pathToMainQml() to get a QUrl to the main QML file
-    //
-    // To display the view, call "show()" (will show fullscreen on device).
-
+int main(int argc, char *argv[]) {
     QScopedPointer<QGuiApplication> app(SailfishApp::application(argc, argv));
     QScopedPointer<QQuickView> view(SailfishApp::createView());
     QQmlContext *context = view.data()->rootContext();
     const char *uri = "harbour.utilities01";
+    const char *internalUri = "harbour.utilities01.internal";
 
     view->engine()->addImportPath(SailfishApp::pathTo("qml/modules").toString()); // Opal
 
     Logic *logic = new Logic(view->engine(), view.data());
     context->setContextProperty("logic", logic);
-    qmlRegisterUncreatableType<Logic>(uri, 1, 0, "Logic", QString());
+    qmlRegisterUncreatableType<Logic>(internalUri, 1, 0, "Logic", QString());
 
     view->setSource(SailfishApp::pathToMainQml());
     view->show();
